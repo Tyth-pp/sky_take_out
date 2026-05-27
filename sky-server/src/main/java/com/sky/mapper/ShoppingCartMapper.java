1efc1605bf82e6c4c4ccbf0a1031225f5465d663
Lill <2736210644@qq.com>
2026-05-27 23:56:25 +0800

    购物车相关接口代码实现

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,40 @@
+package com.sky.mapper;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    /**
+     * 添加购物车
+     * @param shoppingCart
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+    /**
+     * 更新购物车
+     * @param shoppingCart
+     */
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+    /**
+     * 插入购物车数据
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart (name, image, dish_id, setmeal_id, dish_flavor, number, amount, create_time, user_id) values " +
+            "(#{name}, #{image}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime}, #{userId})")
+    void insert(ShoppingCart shoppingCart);
+
+    /**
+     * 清空当前用户的购物车
+     * @param userId
+     */
+
+    @Delete("delete from shopping_cart where user_id = #{userId}")
+    void deleteById(Long userId);
+}
---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,24 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
+
+public interface ShoppingCartService {
+    /**
+     * 显示购物车
+     * @return
+     */
+     List<ShoppingCart> showShoppingCart() ;
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+    /**
+     * 清空购物车
+     */
+    void cleanShoppingCart();
+}
